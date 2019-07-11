51421e4aaa11aa0378e596fed36d3d942f79a5cf
Darren SooHoo <dsoohoo@MossBeachHomes.com>
2019-07-11 12:10:20 -0700

    google auth implemented

--- server.js
+++ server.js
@@ -3,10 +3,29 @@ const connectDB = require('./config/db');
 const path = require('path');
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, 
+    (accessToken, refreshToken, profile, done) => {
+        console.log('access token ' + accessToken);
+        console.log('refresh token ' + refreshToken);
+        console.log('profile: ' + profile);
+    })
+);
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 // client id: 305575279171-qd1s3vloa5sadn5c2krejjprua6mqgcq.apps.googleusercontent.com
 // client secret: mUEPLODYcvDiOUygC0k1TtC3
 // Connect Database
@@ -32,6 +51,9 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
+
+
 
 const PORT = process.env.PORT || 5000;
 
