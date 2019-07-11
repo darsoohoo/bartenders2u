cd5d6a9df65614dc04d5819ce6785eddc204b841
Darren SooHoo <darsoohoo@gmail.com>
2019-07-10 18:17:45 -0700

    updated signin and register page

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -3,7 +3,8 @@ import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { login } from '../../actions/auth';
-import './Login.css';
+import TextField from '@material-ui/core/TextField';
+import './Login.css'
 
 const Login = ({ login, isAuthenticated }) => {
   const [formData, setFormData] = useState({
@@ -27,54 +28,62 @@ const Login = ({ login, isAuthenticated }) => {
 
   return (
     <Fragment>
-      
-            <div id="login-container" >
-              <div id="login-jumbotron" class="jumbotron">    
-                <h1 className='large text-primary'>Sign In</h1>
-                <p className='lead'>
-                  <i className='fas fa-user' /> Sign Into Your Account
-                </p>
-                <form className='form' onSubmit={e => onSubmit(e)}>
-                  <div className='form-group'>
-                    <input
-                      type='email'
-                      placeholder='Email Address'
-                      name='email'
-                      value={email}
-                      onChange={e => onChange(e)}
-                      required
-                    />
-                  </div>
-                  <div className='form-group'>
-                    <input
-                      type='password'
-                      placeholder='Password'
-                      name='password'
-                      value={password}
-                      onChange={e => onChange(e)}
-                      minLength='6'
-                    />
-                  </div>
-                  <input type='submit' className='btn btn-primary' value='Login' />
-                </form>
-                <p className='my-1'>
-                  Don't have an account? <Link to='/register'>Sign Up</Link>
-                </p>
-                </div>
-              </div>
+                <div id="login-container">
+                <div id="login-jumbotron" class="jumbotron">   
+                    <h1 className='large text-primary'>Sign In</h1>
+                    <p className='lead'>
+                        <i className='fas fa-user' /> Sign Into Your Account
+                    </p>
+                    <form className='form' onSubmit={e => onSubmit(e)}>
+              
+                        <div>
+                        <TextField 
+                            onChange={e => onChange(e)}
+                            value={email}
+                            type="email" 
+                            name="email" 
+                            placeholder="Email Address"
+                            margin="normal"
+                            variant="outlined"
+                            label="Email"
+                            className="col-sm-6 login-selection-field"
+                        />
+
+                        <TextField 
+                            onChange={e => onChange(e)}
+                            value={password}
+                            type="password" 
+                            name="password" 
+                            placeholder="Password"
+                            margin="normal"
+                            variant="outlined"
+                            label="Password"
+                            className="col-sm-6 login-selection-field"
+                        />
+                        </div>
+                      
+               
+                     
+                        <input type='submit' className='mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent' value='Login' /> 
+                        <p id="no-account" className='my-1'>
+                        Don't have an account? <Link className=" no-account" to='/register'>Sign Up</Link>
+                        </p>
+                   
+                  
+                    </form>
+  
+                    </div>
+                    </div>
     </Fragment>
   );
 };
-
 Login.propTypes = {
   login: PropTypes.func.isRequired,
   isAuthenticated: PropTypes.bool
 };
-
 const mapStateToProps = state => ({
   isAuthenticated: state.auth.isAuthenticated
 });
-
 export default connect(
   mapStateToProps,
   { login }
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -4,6 +4,7 @@ import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
 import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
+import TextField from '@material-ui/core/TextField';
 import './Register.css'
 
 const Register = ({ setAlert, register, isAuthenticated }) => {
@@ -34,62 +35,69 @@ const Register = ({ setAlert, register, isAuthenticated }) => {
 
   return (
     <Fragment>
-                <div id="quote-container" >
-                <div id="quote-jumbotron" class="jumbotron">            
-
-                  <h1 className='large text-primary'>Sign Up</h1>
-                  <p className='lead'>
-                    <i className='fas fa-user' /> Create Your Account
-                  </p>
-                  <form className='form' onSubmit={e => onSubmit(e)}>
-                    <div className='form-group'>
-                      <input
-                        type='text'
-                        placeholder='Name'
-                        name='name'
-                        value={name}
-                        onChange={e => onChange(e)}
-                      />
+      
+            <div id="register-container">
+                <div id="register-jumbotron" class="jumbotron">    
+                    <h1 className='large text-primary'>Sign Up</h1>
+                    <p className='lead'>
+                        <i className='fas fa-user' /> Create Your Account
+                    </p>
+                    <form className='form' onSubmit={e => onSubmit(e)}>
+                        <div>
+                            <TextField
+                                type='text'
+                                placeholder='Name'
+                                name='name'
+                                value={name}
+                                onChange={e => onChange(e)}
+                                variant="outlined"
+                                label="Name"
+                                className="register-selection-field"
+                            />
+                            <TextField
+                                type='email'
+                                placeholder='Email Address'
+                                name='email'
+                                value={email}
+                                onChange={e => onChange(e)}
+                                variant="outlined"
+                                label="Email"
+                                className="register-selection-field"
+                            />
+                        </div>
+                        <div>
+                            <TextField
+                                type='password'
+                                placeholder='Password'
+                                name='password'
+                                value={password}
+                                onChange={e => onChange(e)}
+                                variant="outlined"
+                                label="Password"
+                                className="register-selection-field"
+                            />
+                    
+                            <TextField
+                                type='password'
+                                placeholder='Confirm Password'
+                                name='password2'
+                                value={password2}
+                                onChange={e => onChange(e)}
+                                variant="outlined"
+                                label="Confirm Password"
+                                className="register-selection-field"
+                            />
+                          
+                        </div>
+            
+                        <input type='submit' className='mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent' value='Register' />
+             
+                    </form>
+                    <p className='my-1'>
+                        Already have an account? <Link to='/login'>Sign In</Link>
+                    </p>
                     </div>
-                    <div className='form-group'>
-                      <input
-                        type='email'
-                        placeholder='Email Address'
-                        name='email'
-                        value={email}
-                        onChange={e => onChange(e)}
-                      />
-                      <small className='form-text'>
-                        This site uses Gravatar so if you want a profile image, use a
-                        Gravatar email
-                      </small>
-                    </div>
-                    <div className='form-group'>
-                      <input
-                        type='password'
-                        placeholder='Password'
-                        name='password'
-                        value={password}
-                        onChange={e => onChange(e)}
-                      />
-                    </div>
-                    <div className='form-group'>
-                      <input
-                        type='password'
-                        placeholder='Confirm Password'
-                        name='password2'
-                        value={password2}
-                        onChange={e => onChange(e)}
-                      />
-                    </div>
-                    <input type='submit' className='btn btn-primary' value='Register' />
-                  </form>
-                  <p className='my-1'>
-                    Already have an account? <Link to='/login'>Sign In</Link>
-                  </p>
-                  </div>
-            </div>
-
+                </div>
     </Fragment>
   );
 };
