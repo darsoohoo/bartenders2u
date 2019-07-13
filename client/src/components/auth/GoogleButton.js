be4262df0333795020ab604fe32eaf1a0b9cfecc
Darren SooHoo <darsoohoo@gmail.com>
2019-07-12 18:22:15 -0700

    added googlebutton component

---
+++ client/src/components/auth/GoogleButton.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react';
+
+const GoogleButton = () => {
+
+
+
+
+
+
+
+}
+
+return (
+    <Fragment>
+    
+            successful sign in -->
+            <div id="gSignInWrapper">
+                <span class="label">Sign in with:</span>
+                <div id="customBtn" class="customGPlusSignIn">
+                <span class="icon"></span>
+                <span class="buttonText">Google</span>
+                </div>
+            </div>
+            <div id="name"></div>
+
+
+    </Fragment>
+    
+)
+
+export default GoogleButton
