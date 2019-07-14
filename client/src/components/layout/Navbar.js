bfa4a53b09f096062428dd6e9213e3cf9940884b
Darren SooHoo <darsoohoo@gmail.com>
2019-07-13 17:45:45 -0700

    signout successful

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,11 +3,14 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import './Navbar.css'
-
-
 import { getCurrentProfile, deleteAccount } from '../../actions/profile';
+import { logout } from '../../actions/auth';
+
+
+
 
 const Navbar = ({
+  logout,
   getCurrentProfile,
   auth: { user },
   profile: { profile, loading }
@@ -16,28 +19,36 @@ const Navbar = ({
     getCurrentProfile();
   }, [getCurrentProfile]);
 
+  const Logout = () => {
+    logout()
+    window.location.reload();
+  }
+
   return  user !== null ? (
     <nav className="navbar navbar-default navbar-fixed-top">
-    <div className="container-fluid">
-        <div className="navbar-header">
-
+    <div className="leftside-nav container-fluid">
+        <div className=" navbar-header">
         <Link className="navbar-brand" to='/'>B2U</Link>
         </div>
         <div>
         <ul className="nav navbar-nav navbar-left">
-        
             <li><Link to='/packages'>Our Services</Link></li>
             <li><Link to='/company'>Our Company</Link></li>
         </ul>
         </div>
         <div className="collapse navbar-collapse" id="myNavbar">
-        <ul className="nav navbar-nav navbar-right">
+        <ul className="rightside-nav nav navbar-nav navbar-right">
+          <li><img className="nav-avatar" src={user.avatar}/></li>
             <li className="dropdown">
-            <a className="dropdown-toggle" data-toggle="dropdown" href="#">{user.name}
+            <a className="dropdown-toggle" data-toggle="dropdown" href="#">
+            {
+              (user.name.charAt(0).toUpperCase() + user.name.slice(1) )
+              
+              }
             <span className="caret"></span></a>
             <ul className="dropdown-menu">
                 <li><Link to="/dashboard">Account</Link></li>
-                <li><Link to="/signout">Signout</Link></li>
+                <li><Link onClick={Logout}>Signout</Link></li>
             </ul>
             </li>
         </ul>
@@ -51,19 +62,18 @@ const Navbar = ({
 
         <nav className="navbar navbar-default navbar-fixed-top">
                         <div className="container-fluid">
-                            <div className="navbar-header">
+                            <div className="leftside-nav navbar-header">
 
                             <Link className="navbar-brand" to='/'>B2U</Link>
                             </div>
                             <div>
-                            <ul className="nav navbar-nav navbar-left">
-                            
+                            <ul className="leftside-nav nav navbar-nav navbar-left">
                                 <li><Link to='/packages'>Our Services</Link></li>
                                 <li><Link to='/company'>Our Company</Link></li>
                             </ul>
                             </div>
                             <div className="collapse navbar-collapse" id="myNavbar">
-                            <ul className="nav navbar-nav navbar-right">
+                            <ul className="rightside-nav nav navbar-nav navbar-right">
                                 <li><Link to='/login'>Login</Link></li>
                                 <li><Link to='/register'>Signup</Link></li>
                             </ul>
@@ -77,17 +87,20 @@ const Navbar = ({
 };
 
 Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
   getCurrentProfile: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
-  profile: PropTypes.object.isRequired
+  profile: PropTypes.object.isRequired,
+  isAuthenticated: PropTypes.bool
 };
 
 const mapStateToProps = state => ({
   auth: state.auth,
-  profile: state.profile
+  profile: state.profile,
+  isAuthenticated: state.auth.isAuthenticated
 });
 
 export default connect(
   mapStateToProps,
-  { getCurrentProfile }
+  { getCurrentProfile, logout },
 )(Navbar);
